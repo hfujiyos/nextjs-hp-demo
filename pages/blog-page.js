36491fd98f38f6ab762c270c5dfbe19a27240b78
hfujiyos <hfujiyos@gmail.com>
2022-05-15 16:42:12 +0900

    added feature/blog

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,6 +1,30 @@
-import Layout from "../components/Layout";
-const Blog = () => {
-  return <Layout title="Blog">Blog page</Layout>;
-};
+import Layout from "../components/Layout";      //Lv4-Layoutｺﾝﾎﾟｰﾈﾝﾄ
+import Post from "../components/Post";          //ﾌﾞﾛｸﾞ行ｺﾝﾎﾟｰﾈﾝﾄ
+import { getAllPostsData } from "../lib/posts"; //ﾌﾞﾛｸﾞAPI取得ﾗｲﾌﾞﾗﾘ
 
-export default Blog;
+/**
+ * Blogｺﾝﾎﾟｰﾈﾝﾄ
+ * @param posts ﾌﾞﾛｸﾞAPI結果ｾｯﾄ（JSON）
+ * @description ﾌﾞﾛｸﾞ情報を取得してPostｺﾝﾎﾟｰﾈﾝﾄで全件表示
+ */
+export default function Blog({ posts }) {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
+}
+
+/**
+ * Propsｻｰﾊﾞｰｻｲﾄﾞ関数
+ * @returns props｜posts｜JSONﾃﾞｰﾀ
+ * @description ﾋﾞﾙﾄﾞ時ｻｰﾊﾞｰｻｲﾄﾞでgetAllPostsDataを実行してSSG
+ */
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
